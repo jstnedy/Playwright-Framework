b30e0ad3edfa28e0e88ec3e44e1cee16bea54464
Justine Agner <Justine.agner1@gmail.com>
2026-02-19 15:25:59 +0800

    Fix CI: headless on GitHub

--- playwright.config.js
+++ playwright.config.js
@@ -1,24 +1,25 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  workers: 5,
   fullyParallel: true,
-  retries: 1,
+  retries: process.env.CI ? 1 : 1,
   timeout: 90000,
   expect: { timeout: 90000 },
 
+  workers: process.env.CI ? 2 : 5,
+
   reporter: [
-    ['html', { open: 'always' }],
-    ['allure-playwright', {open: 'always'}],
+    ['html', { open: process.env.CI ? 'never' : 'always' }],
+    ['allure-playwright'],
   ],
 
-  use: {
-    browserName: 'chromium',
-    headless: false,
-    screenshot: 'on',
-    video: 'on',
-    trace: 'on',
-  },
+ use: {
+  browserName: 'chromium',
+  headless: !!process.env.CI,          // headed locally, headless in CI
+  screenshot: 'only-on-failure',
+  video: 'retain-on-failure',
+  trace: 'on-first-retry',
+},
 });
